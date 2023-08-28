4756b7f6093fa56d3e273f8633b511b4c44d8bdf
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-08-28 21:42:06 +0300

    fix

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -4,15 +4,22 @@
   const closeMenuBtn = document.querySelector(".js-close-menu");
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    const isMenuOpen = mobileMenu.classList.contains("is-open");
+    
+    if (isMenuOpen) {
+      mobileMenu.classList.remove("is-open");
+      mobileMenu.classList.add("is-closed");
+    } else {
+      mobileMenu.classList.remove("is-closed");
+      mobileMenu.classList.add("is-open");
+    }
+
     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-    mobileMenu.classList.toggle("is-open");
 
     const scrollLockMethod = !isMenuOpen
-      ? "disableBodyScroll"
-      : "enableBodyScroll";
-    bodyScrollLock[scrollLockMethod](document.body);
+      ? bodyScrollLock.disableBodyScroll
+      : bodyScrollLock.enableBodyScroll;
+    scrollLockMethod(document.body);
   };
 
   const scrollToAnchor = () => {
@@ -23,7 +30,6 @@
   openMenuBtn.addEventListener("click", toggleMenu);
   closeMenuBtn.addEventListener("click", toggleMenu);
 
-  // Обробка події для всіх посилань з класом .js-scroll-to-anchor
   const scrollToAnchorLinks = document.querySelectorAll(".js-scroll-to-anchor");
   scrollToAnchorLinks.forEach(link => {
     link.addEventListener("click", scrollToAnchor);
@@ -31,7 +37,7 @@
 
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
-    mobileMenu.classList.remove("is-open");
+    mobileMenu.classList.remove("is-open", "is-closed");
     openMenuBtn.setAttribute("aria-expanded", false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
